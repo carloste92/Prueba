2d156f12aa752cc7c02d89ca5b3a9f8eaa65957b
carloste92 <carloste92@hotmail.com>
2014-03-31 19:13:29 -0500

    Implementando Capa de Datos,GEstion de BD

---
+++ ApsParametro/Datos/clsConexion.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CJ
+ * Date: 31/03/2014
+ * Time: 5:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Datos
+{
+	/// <summary>
+	/// Description of clsConexion.
+	/// </summary>
+	public class clsConexion
+	{
+		static String _cadconexion=@"Data Source=ZAY;Initial Catalog=prueba;User ID=ZAY\CJ;Password=";
+		public static String cadconexion{
+			get{
+				return _cadconexion;
+			}
+		}
+	}
+}
---
+++ ApsParametro/Datos/clsGestionBD.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CJ
+ * Date: 31/03/2014
+ * Time: 5:31
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Datos
+{
+	/// <summary>
+	/// Description of clsGestionBD.
+	/// </summary>
+	public class clsGestionBD
+	{
+		public clsGestionBD()
+		{
+			
+		}
+		public static SqlCommand CrearComando()
+		{
+		    String cadconexion = clsConexion.cadconexion;
+		    SqlConnection Con = new SqlConnection();
+		 	Con.ConnectionString=cadconexion;
+		    SqlCommand Com= new SqlCommand();
+		    Com = Con.CreateCommand();
+		    Com.CommandType = CommandType.Text;
+		    return Com;
+		}
+		public static SqlCommand CrearComandoProcedimiento(String PA)
+		{
+		    String cadconexion = clsConexion.cadconexion;
+		    SqlConnection Con = new SqlConnection();
+		 	Con.ConnectionString=cadconexion;
+		    SqlCommand Com= new SqlCommand();
+		    Com = Con.CreateCommand();
+		    Com.CommandType = CommandType.StoredProcedure;
+		    return Com;
+		}
+	}
+}
